1675b6bb446cb1c97cf5042f572eba7773be5168
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-12-29 20:27:48 -0300

    day8:LeetCode Parenthesis Challenge using Deque, pop, push and peek and brute force sliding windows...

--- src/HackerRank/SlidingWindows.java
+++ src/HackerRank/SlidingWindows.java
@@ -1,62 +1,66 @@
 package HackerRank;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author rferreira
- * You are given an array of integers nums, there is a sliding window of size k
+ * You are given an array of integers nums, there is a sliding window of size iteratorOfNumbers
  * which is moving from the very left of the array to the very right.
- * You can only see the k numbers in the window.
+ * You can only see the iteratorOfNumbers numbers in the window.
  * Each time the sliding window moves right by one position.
- * Return the max sliding window.
+ * Return the maxOfEachArray sliding window.
  * Example 1:
- * Input: nums = [1,3,-1,-3,5,3,6,7,8], k = 3
+ * Input: nums = [1,3,-1,-3,5,3,6,7,8], iteratorOfNumbers = 3
  * Output: [3,3,5,5,6,7]
  * Explanation:
  * Window position                Max
  * ---------------               -----
- * [1  3  -1] -3  5  3  6  7  8       3
- *  1 [3  -1  -3] 5  3  6  7  8     3
- *  1  3 [-1  -3  5] 3  6  7  8    5
- *  1  3  -1 [-3  5  3] 6  7  8    5
- *  1  3  -1  -3 [5  3  6] 7  8    6
- *  1  3  -1  -3  5 [3  6  7] 8    7
- *  1  3  -1  -3  5 3  [6  7   8]      7
+ * [1  3  -1] -3  5  3  6  7  8    3
+ * 1 [3  -1  -3] 5  3  6  7  8    3
+ * 1  3 [-1  -3  5] 3  6  7  8    5
+ * 1  3  -1 [-3  5  3] 6  7  8    5
+ * 1  3  -1  -3 [5  3  6] 7  8    6
+ * 1  3  -1  -3  5 [3  6  7] 8    7
+ * 1  3  -1  -3  5  3  [6 7  8]   8
  */
 public class SlidingWindows {
 
   public static void main(String[] args) {
-    int[] x = new int[]{1,2,3,4,5,6,7,8,9};
-    int k =3;
-    returnMaxWindow(x,k);
+    int[] x = new int[]{1, 3, -1, -3, 5, 3, 6, 7, 8};
+    int iteratorOfNumbers = 3;
+    int[] maximumOfEachWindow = new int[x.length-2];
+    maximumOfEachWindow = returnMaxWindow(x,iteratorOfNumbers);
+    for (int i : maximumOfEachWindow) {
+      System.out.print(i+" ");
+    }
   }
 
   public static int[] returnMaxWindow(int[] numbers, int window) {
-    int size = window;
-    int[][] slidingWindow = new int[numbers.length-2][size];
+    int numberOfArrays = numbers.length - 2;
+    int[][] slidingWindow = new int[numberOfArrays][window];
+    int[] maximumOfEachArray = new int[numberOfArrays];
+    int maxOfEachArray = 0;
+    int incrementOnNumbers = 0;
+    int iteratorOfNumbers = 0;
 
-
-    int c =0;
-    int temp = window;
-    System.out.println(Arrays.toString(slidingWindow));
-    for ( int[] m : slidingWindow) {
-      while (temp > 0) {
-        System.out.println(c +" " + temp);
-        m[c] = numbers[c];
-        c++;
-        temp--;
+    for (int i = 0; i < slidingWindow.length; i++) {
+      for (int j = 0; j < slidingWindow[i].length; j++) {
+        slidingWindow[i][j] = numbers[iteratorOfNumbers];
+        iteratorOfNumbers++;
       }
-      c=0;
-      temp=window;
-      System.out.println(Arrays.toString(m));
+      incrementOnNumbers++;
+      iteratorOfNumbers = incrementOnNumbers;
     }
 
-
-
-    int[] windows = new int[]{};
-//    windows = slidingWindow.toArray();
-    return windows;
+    for (int i = 0; i < slidingWindow.length; i++) {
+      for (int j = 0; j < slidingWindow[j].length; j++) {
+        if (slidingWindow[i][j] > maxOfEachArray) {
+          maxOfEachArray = slidingWindow[i][j];
+        }
+        maximumOfEachArray[i] = maxOfEachArray;
+      }
+    }
+    return maximumOfEachArray;
   }
 }
+
