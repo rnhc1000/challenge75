a3b4c4fda9d1e74ec6ab05a1416c9dbef41e73c7
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-01-30 23:56:10 -0300

    Stack, Queue and LeetCode Daily Challenge Reverse Polish Notation

--- src/udemy/dsa/doublyLinkedList/Main.java
+++ src/udemy/dsa/doublyLinkedList/Main.java
@@ -6,13 +6,13 @@ public class Main {
   public static void main(String[] args) {
     int value = 20;
 
-    DoublyLinkedList dbLList = new DoublyLinkedList(1);
+    DoublyLinkedListDoubly dbLList = new DoublyLinkedListDoubly(1);
     for (int i = 5; i < value; i += 2) {
       dbLList.append(i);
     }
     dbLList.printAll();
     System.out.println();
-    DoublyLinkedList.Node currentNode = dbLList.getHead();
+    DoublyLinkedListDoubly.Node currentNode = dbLList.getHead();
     System.out.println("Head: " + currentNode.value);
     currentNode = dbLList.getTail();
     System.out.println("Tail: " + currentNode.value);
@@ -35,6 +35,75 @@ public class Main {
     dbLList.printAll();
     System.out.println(dbLList.remove(4));
     dbLList.printAll();
+    DoublyLinkedListDoubly myDLL = new DoublyLinkedListDoubly(1);
+    myDLL.append(2);
+    myDLL.append(3);
+    myDLL.append(4);
+    myDLL.append(5);
+
+    System.out.println("DLL before remove():");
+    myDLL.printList();
+
+    System.out.println("\nRemoved node:");
+    System.out.println(myDLL.remove(2).value);
+    System.out.println("DLL after remove() in middle:");
+    myDLL.printList();
+
+    System.out.println("\nRemoved node:");
+    System.out.println(myDLL.remove(0).value);
+    System.out.println("DLL after remove() of first node:");
+    myDLL.printList();
+
+    System.out.println("\nRemoved node:");
+    System.out.println(myDLL.remove(2).value);
+    System.out.println("DLL after remove() of last node:");
+    myDLL.printList();
+
+    System.out.println("DLL before swap:");
+    myDLL.printList();
+
+    myDLL.swapFirstLast();
+
+    System.out.println("\nDLL after swap:");
+    myDLL.printList();
+
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            DLL before remove():
+            1
+            2
+            3
+            4
+            5
+
+            Removed node:
+            3
+            DLL after remove() in middle:
+            1
+            2
+            4
+            5
+
+            Removed node:
+            1
+            DLL after remove() of first node:
+            2
+            4
+            5
+
+            Removed node:
+            5
+            DLL after remove() of last node:
+            2
+            4
+
+        */
+
   }
+
 }
+
 
