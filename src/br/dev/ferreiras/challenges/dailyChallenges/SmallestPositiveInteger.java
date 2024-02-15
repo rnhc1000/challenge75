a28622b9ea5b7c23d0e02ffedae658e1b1f1c099
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-02-15 00:53:36 -0300

    day46Of24 - PriorityQueue<Integer>((x,y)->x-y) or ((x,y)->y-x) ascending order or descending order

--- src/br/dev/ferreiras/challenges/dailyChallenges/SmallestPositiveInteger.java
+++ src/br/dev/ferreiras/challenges/dailyChallenges/SmallestPositiveInteger.java
@@ -8,7 +8,8 @@ import java.util.Set;
  * class Solution { public int solution(int[] A); }
  * that, given an array A of N integers, returns the smallest positive integer
  * (greater than 0) that does not occur in A.
- * For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+ * For example,
+ * Given A = [1, 3, 6, 4, 1, 2], the function should return 5.
  * Given A = [1, 2, 3], the function should return 4.
  * Given A = [−1, −3], the function should return 1.
  * Write an efficient algorithm for the following assumptions:
