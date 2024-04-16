56765468b619dd113700550c8994368c7c8f86fe
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-04-16 20:13:36 -0300

    day107Of24 - Iterating on a tree, inserting a node and a row of nodes at a given depth

--- src/br/dev/ferreiras/udemy/dsa/binarytree/binarysearchtree/Main.java
+++ src/br/dev/ferreiras/udemy/dsa/binarytree/binarysearchtree/Main.java
@@ -2,6 +2,8 @@ package br.dev.ferreiras.udemy.dsa.binarytree.binarysearchtree;
 
 import java.util.List;
 
+import static br.dev.ferreiras.udemy.dsa.binarytree.binarysearchtree.BinarySearchTree.*;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -17,6 +19,18 @@ public class Main {
     myBST.insert(52);
     myBST.insert(82);
 
+
+    TreeNode root = newNode(1);
+    root.left = newNode(2);
+    root.right = newNode(3);
+
+    root.left.left = newNode(4);
+    root.left.right = newNode(5);
+
+    System.out.println("Height(Depth) of tree is: "
+            + height(root));
+
+
     System.out.println(myBST);
     System.out.println("BST Contains 27:");
     System.out.println(myBST.recursiveContains(27));
@@ -24,25 +38,29 @@ public class Main {
     System.out.println(myBST.recursiveContains(17));
     System.out.println("BreadthFirstSearch");
     List<Integer> result = myBST.breadthFirstSearch();
-    for (Integer i : result) {
+    for (
+            Integer i : result) {
       System.out.print(i + " ");
     }
     System.out.println("\n-------------------------------");
     System.out.println("DepthFirstSearch-PreOrder ");
     result = myBST.depthFirstSearchPreOrder();
-    for (Integer i : result) {
+    for (
+            Integer i : result) {
       System.out.print(i + " ");
     }
     System.out.println("\n-------------------------------");
     System.out.println("DepthFirstSearch-PostOrder ");
     result = myBST.depthFirstSearchPostOrder();
-    for (Integer i : result) {
+    for (
+            Integer i : result) {
       System.out.print(i + " ");
     }
     System.out.println("\n-------------------------------");
     System.out.println("DepthFirstSearch-InOrder ");
     result = myBST.depthFirstSearchInOrder();
-    for (Integer i : result) {
+    for (
+            Integer i : result) {
       System.out.print(i + " ");
     }
         /*
