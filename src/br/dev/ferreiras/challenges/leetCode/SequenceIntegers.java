69ade2c318144018291bb97cd2f5ceec04fb9778
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-06-03 11:41:02 -0300

    day155Of24 - A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters - StringSubsequence leetcode challenge

--- src/br/dev/ferreiras/challenges/leetCode/SequenceIntegers.java
+++ src/br/dev/ferreiras/challenges/leetCode/SequenceIntegers.java
@@ -2,7 +2,7 @@ package br.dev.ferreiras.challenges.leetCode;
 
 import java.util.*;
 
-/**
+/*
  * An integer has sequential digits if and only if each digit in the number
  * is one more than the previous digit.
  * Return a sorted list of all the integers in the range [low, high] inclusive
